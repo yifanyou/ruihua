de375a391220259d78bfb41c60534d5c160dc300
YiFan <kiss2041@126.com>
2015-08-25 12:50:32 +0800

    first run successfully

--- src/main/java/com/yifan/ruihua/controller/SalaryController.java
+++ src/main/java/com/yifan/ruihua/controller/SalaryController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SalaryController {
-	@RequestMapping("")
+	@RequestMapping("/salary")
 	public String show(){
-		return "";
+		return "views/salary";
 	}
 }
